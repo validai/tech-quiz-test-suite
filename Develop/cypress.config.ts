cc57ee03635ca21f3c38841bc0f38c79d7cdf0c3
validai <djcoast239@gmail.com>
2025-03-09 21:17:28 -0400

    Setup cypress configuration

--- Develop/cypress.config.ts
+++ Develop/cypress.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     },
     specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
   },
+
   e2e: {
     baseUrl: 'http://localhost:5173',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
